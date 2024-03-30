39acb6ac2e1f0bd7a0e00793e6b56fae7e98f9d7
SHAHBAZ HAIDER MIRZA <119895955+Shahbazmirza1@users.noreply.github.com>
2024-03-31 01:52:13 +0500

    Delete vsm directory

--- vsm/mapper1.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-import sys
-
-for line in sys.stdin:
-    line = line.strip()
-    article_id, tf_idf_list_str = line.split(':')
-    print(f"{article_id}\t{tf_idf_list_str}")
-
